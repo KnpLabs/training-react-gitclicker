cec5954e451eb1ecaf2dfa740859776929b19cbc
ErwannRousseau <erwann.rousseau@icloud.com>
2025-03-20 18:13:10 +0100

    feat: jsx example

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,32 @@
 import { createRoot } from "react-dom/client";
 
+const user = {
+  id: 1,
+  name: "John Doe",
+};
+
+function capitalize(name) {
+  return name.toUpperCase();
+}
+
+function getAvatarUrl(user) {
+  return `https://api.dicebear.com/9.x/adventurer/svg?seed=${user.name}${user.id}`;
+}
+
+const greeting = (
+  <div>
+    <h1>Hello {capitalize(user.name)}</h1>
+    <img src={getAvatarUrl(user)} alt={`${user.name}'s avatar`} />
+  </div>
+);
+
+const header = (
+  <header>
+    <img src="https://picsum.photos/id/237/200/300" alt="picsum" />
+    {greeting}
+  </header>
+);
+
 const root = createRoot(document.getElementById("root"));
 
-root.render(<div />);
+root.render(header);
