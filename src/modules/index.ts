7a2edeeb57be7c893f089476d94c500da1467b92
ErwannRousseau <erwann.rousseau@icloud.com>
2025-03-20 17:45:18 +0100

    chore: delete CRA

--- src/modules/index.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit'
-import game from './game'
-import rules from './rules'
-
-export const rootReducer =  combineReducers({
-    game: game.reducer,
-    rules: rules.reducer
-})
