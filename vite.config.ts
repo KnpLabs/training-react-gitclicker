54536cebd6dc0bd52b09f8d3718900fc724292f1
ErwannRousseau <erwann.rousseau@icloud.com>
2025-04-02 18:33:42 +0200

    feat: setup Vitest and adding tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import path from 'node:path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
@@ -8,6 +9,14 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test-setup.ts',
+    coverage: {
+      reporter: ['text'],
     },
   },
 })
