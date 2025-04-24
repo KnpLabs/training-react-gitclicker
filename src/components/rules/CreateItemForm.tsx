7a2186251ae7a73eae6b9331f301a5a6c7ff852d
ErwannRousseau <erwann.rousseau@icloud.com>
2025-04-24 11:05:55 +0200

    feat: add create item form and handle submission

--- src/components/rules/CreateItemForm.tsx
+++ src/components/rules/CreateItemForm.tsx
@@ -1,7 +1,85 @@
+import '@/styles/rules/form.css'
+import { useState } from 'react'
+import { useAppDispatch } from '@/store'
+import { addItem } from '@/modules/rules'
+import { InputLabel, Input, Button } from '@mui/material'
+import AddIcon from '@mui/icons-material/Add'
+
+type FormValues = {
+  name: string
+  price: number
+  linesPerMillisecond: number
+}
+
 export function CreateItemForm() {
+  const dispatch = useAppDispatch()
+
+  const [formValues, setFormValues] = useState<FormValues>({
+    name: '',
+    price: 0,
+    linesPerMillisecond: 0,
+  })
+
+  const handleChange: React.ComponentProps<'input'>['onChange'] = (e) => {
+    const { name, value } = e.target
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    })
+  }
+
+  const handleSubmit: React.ComponentProps<'form'>['onSubmit'] = (e) => {
+    e.preventDefault()
+    dispatch(addItem(formValues))
+  }
+
   return (
-    <div>
-      CREATE ITEM FORM
-    </div>
+    <form onSubmit={handleSubmit} className="form">
+      <div>
+        <InputLabel htmlFor="name">Name *</InputLabel>
+        <Input
+          id="name"
+          className="input"
+          type="text"
+          name="name"
+          placeholder="Item Name"
+          value={formValues.name}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <InputLabel htmlFor="price">Price *</InputLabel>
+        <Input
+          id="price"
+          className="input"
+          type="number"
+          name="price"
+          value={formValues.price}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <InputLabel htmlFor="linesPerMillisecond">Lines per millisecond *</InputLabel>
+        <Input
+          id="linesPerMillisecond"
+          className="input"
+          type="number"
+          name="linesPerMillisecond"
+          inputProps={{
+            step: 0.1,
+          }}
+          value={formValues.linesPerMillisecond}
+          onChange={handleChange}
+        />
+      </div>
+      <Button
+        type="submit"
+        variant="contained"
+        startIcon={<AddIcon />}
+        color="primary"
+      >
+        Add Item
+      </Button>
+    </form>
   )
 }
--- src/modules/rules.ts
+++ src/modules/rules.ts
@@ -21,6 +21,24 @@ export const fetchItems = createAsyncThunk(
   },
 )
 
+export const addItem = createAsyncThunk(
+  'rules/addItem',
+  async (itemData: Omit<Item, 'id'>, { dispatch }) => {
+    const response = await fetch(`${import.meta.env.VITE_API_URL}/api/shop/items`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(itemData),
+    })
+
+    const newItem = await response.json() as Item
+
+    dispatch(itemReceived(newItem))
+  },
+)
+
 const rules = createSlice({
   name: 'rule',
   initialState: INITIAL_STATE,
@@ -28,11 +46,15 @@ const rules = createSlice({
     fetchedItems: (state, action: PayloadAction<Item[]>) => {
       state.items = action.payload
     },
+    itemReceived: (state, action: PayloadAction<Item>) => {
+      state.items.push(action.payload)
+    },
   },
 })
 
 const {
   fetchedItems,
+  itemReceived,
 } = rules.actions
 
 export {
